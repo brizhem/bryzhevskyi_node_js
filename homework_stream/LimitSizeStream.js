25aca4b0fe90a0e500a0dde93eb90a74b74f420b
Yevgeniy.B <brizhem@gmail.com>
2022-05-04 13:21:02 +0300

    homework stream fixed problems

--- homework_stream/LimitSizeStream.js
+++ homework_stream/LimitSizeStream.js
@@ -3,7 +3,6 @@ const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
-    options.writableHighWaterMark = options.limit;
     super(options);
     this._limit = options.limit;
     this.count = 0;
@@ -12,7 +11,7 @@ class LimitSizeStream extends stream.Transform {
   _transform(chunk, encoding, callback) {
     this.count += Buffer.byteLength(chunk);
     if (this.count > this._limit) {
-      callback(LimitExceededError.code);
+      callback(new LimitExceededError());
     } else {
       callback(null, chunk);
     }
